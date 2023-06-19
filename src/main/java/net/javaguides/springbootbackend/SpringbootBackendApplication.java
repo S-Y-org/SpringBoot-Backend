c283dd93c5fd8fc6b888bf8f8685ad0bb392a221
IT21440236 <it21440236@my.sliit.lk>
2023-06-19 14:54:33 +0530

    backend

--- src/main/java/net/javaguides/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springbootbackend/SpringbootBackendApplication.java
@@ -1,21 +1,39 @@
 package net.javaguides.springbootbackend;
 
+import net.javaguides.springbootbackend.model.Employee;
+import net.javaguides.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication implements CommandLineRunner {
+public class SpringbootBackendApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }
+
+//	@Autowired
+//	private EmployeeRepository employeeRepository;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Employee employee = new Employee();
+//		employee.setFirstName("Yeran");
+//		employee.setLastName("Kods");
+//		employee.setEmailId("yk@gmail.com");
+//		employeeRepository.save(employee);
+//
+//		Employee employee1 = new Employee();
+//		employee1.setFirstName("Dwayne");
+//		employee1.setLastName("Johnson");
+//		employee1.setEmailId("dj@gmail.com");
+//		employeeRepository.save(employee1);
+//
+//	}
+//}
 
 
 //NOTES
@@ -51,4 +69,76 @@ The CommandLineRunner interface is useful in scenarios where you need to perform
 You can have multiple implementations of the CommandLineRunner interface in your application, and they will be executed in the order defined by the @Order annotation or by implementing the Ordered interface.
 
 Note: The CommandLineRunner interface is specific to Spring Boot and provides a convenient way to execute code during application startup. In regular Spring applications, you can use the ApplicationRunner interface, which has a similar purpose but provides a different callback method (run(ApplicationArguments args)) with additional functionality for parsing command-line arguments.
+
+
+====================================================================================
+Why is there three dots here ; (String... args)?
+
+The three dots (...) in (String... args) represent varargs (variable arguments) in Java. It allows you to pass a variable number of arguments of the specified type to a method.
+
+In the case of CommandLineRunner, the run() method accepts command-line arguments as input. The varargs parameter args allows you to pass multiple command-line arguments to the method.
+
+Here's how it works:
+
+When you run a Spring Boot application from the command line, you can pass additional arguments after the application name. For example:
+
+java -jar myapp.jar arg1 arg2 arg3
+
+The args parameter in the run() method is an array of strings that captures these command-line arguments. The varargs notation (String... args) indicates that you can pass zero or more strings as arguments.
+
+If no additional command-line arguments are provided, args will be an empty array (args.length will be 0).
+If command-line arguments are provided, they will be stored in the args array. Each argument will be represented as a separate element in the array.
+For example, if you run your Spring Boot application as follows:
+
+java -jar myapp.jar arg1 arg2 arg3
+
+
+The args array in the run() method will be populated as ["arg1", "arg2", "arg3"].
+
+By using varargs, you have the flexibility to handle different numbers of command-line arguments without explicitly specifying the size of the array. You can iterate over the args array or access specific elements based on your application's requirements.
+
+Note: The varargs notation can be used with any type, not just String. For example, you can have a method with int... numbers to accept a variable number of integer arguments.
+
+
+
+==============================================================================
+
+When we want to hard code and create an employee we use the CommandLineRunner interface and implement its run method ;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Yeran");
+		employee.setLastName("Kods");
+		employee.setEmailId("yk@gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Dwayne");
+		employee1.setLastName("Johnson");
+		employee1.setEmailId("dj@gmail.com");
+		employeeRepository.save(employee1);
+
+	}
+}
+
+
+
+
+
+
+
+
+
 */
--- src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -1,16 +1,17 @@
 package net.javaguides.springbootbackend.controller;
 
+import net.javaguides.springbootbackend.exception.ResourceNotFoundException;
 import net.javaguides.springbootbackend.model.Employee;
 import net.javaguides.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/employee")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     @Autowired
@@ -19,6 +20,72 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    //build CREATE employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    //@RequestBody will basically convert a JSON into a java object
+
+    //build GET employee by id
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id: " +id));
+        return ResponseEntity.ok(employee);
+
+    }
+    /*
+    @GetMapping("{id}"): This annotation specifies that the method handles HTTP GET requests for a specific URL pattern. In this case, the URL pattern includes a path variable {id} which represents the unique identifier of the employee.
+
+public ResponseEntity<Employee> getEmployeeById(@PathVariable long id): This method accepts a long parameter id annotated with @PathVariable. The @PathVariable annotation is used to bind the value of the path variable from the URL to the id parameter.
+
+Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));: This line retrieves the employee from the employeeRepository by calling the findById() method, passing in the id parameter. If the employee with the given id is not found, it throws a custom exception ResourceNotFoundException with a specific error message.
+
+return ResponseEntity.ok(employee);: If the employee is found, this line returns a response with the HTTP status code 200 (OK) and the employee object in the response body. The ResponseEntity.ok() method is used to create a ResponseEntity instance with the specified body and status.
+
+In summary, this controller method handles a GET request to retrieve an employee by their unique identifier. It fetches the employee from the repository based on the provided id, throws an exception if the employee is not found, and returns the employee object in the response if it exists. The response is wrapped in a ResponseEntity object, allowing you to set additional response headers and status codes if needed.
+
+The ()-> syntax you see in the code snippet is part of a lambda expression in Java.
+
+The ()-> represents an anonymous function or lambda function with no parameters. It's shorthand for declaring a functional interface (an interface with a single abstract method) and implementing its method on the fly.
+
+The lambda expression is used as an argument to the orElseThrow() method, which is a method provided by the Optional class in Java. The purpose of orElseThrow() is to either return the value contained within the Optional if it exists or throw an exception if it's empty.
+
+In this case, if the findById(id) method returns an empty Optional (meaning no employee was found with the given id), the lambda expression () -> new ResourceNotFoundException("Employee not exist with id: " + id) is executed. It creates a new instance of the ResourceNotFoundException class with a specific error message. This exception is then thrown, terminating the execution and indicating that the requested resource (employee) was not found.
+
+The lambda expression syntax ()-> allows you to define a block of code as an inline function without explicitly creating a separate method or implementing an interface. It provides a concise way to define and use simple functions in Java.
+     */
+
+
+    //build UPDATE employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee not exist with id: "+ id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+
+    }
+
+
+    //build DELETE employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id:" + id));
+
+        employeeRepository.delete(employee);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
 
@@ -74,9 +141,30 @@ In this example, the EmployeeService class is annotated with @Repository, indica
 By using @Autowired, you can delegate the responsibility of instantiating and managing dependencies to the Spring framework. It helps to decouple components and enables easy integration of different parts of your application.
 
 
+=============================================================================================
 
+The @RequestBody annotation in Spring is used to bind the HTTP request body to a method parameter in a controller handler method. It is commonly used in RESTful web services to extract data from the request body and convert it into an object.
 
+When a request is made to a Spring controller method, the @RequestBody annotation tells Spring to bind the contents of the request body to the annotated parameter. Spring automatically performs the conversion based on the content type of the request, such as JSON or XML.
 
+Here's an example:
+
+@PostMapping("/users")
+public ResponseEntity<User> createUser(@RequestBody User user) {
+    // Process the user object received from the request body
+    // ...
+    return ResponseEntity.ok(user);
+}
+
+In this example, the createUser method handles a POST request to the "/users" endpoint. The @RequestBody annotation is applied to the User parameter, indicating that the user data should be extracted from the request body and mapped to the User object.
+
+When a POST request is sent to this endpoint with a JSON body containing user data, Spring automatically converts the JSON payload into a User object and binds it to the user parameter.
+
+The @RequestBody annotation can be used with different types of parameters, such as custom Java objects, collections, or simple types like strings and numbers. It simplifies the process of extracting request data and avoids manual parsing and conversion.
+
+It's important to note that the @RequestBody annotation is typically used in combination with other annotations, such as @PostMapping, to define the request mapping and HTTP method for the controller method.
+
+Overall, @RequestBody helps in processing the request body data in a convenient and type-safe manner, allowing you to work with complex objects seamlessly in your Spring controllers.
 
 
 
